a06fc7b40008b4f9220617c8dd9f279d652220f4
pankaj <pankaj.p.mutha14@gmail.com>
2025-03-22 18:43:09 +0530

    Hello world app

--- src/main/java/com/demo_dockers/controller/HelloWorldExample.java
+++ src/main/java/com/demo_dockers/controller/HelloWorldExample.java
@@ -1,13 +1,15 @@
 package com.demo_dockers.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldExample {
 
-    @RequestMapping(value = "/message")
+    @GetMapping
     public String getMessage(){
+        System.out.println("##########################");
         return "Hello World from psa";
     }
 }
